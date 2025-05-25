6f529118a2f30d92285fd54ac88f2b91e859042f
向井航平 <mukaikouhei@mukaikouheinoMacBook-Air.local>
2025-05-25 23:14:44 +0900

    change:routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :map, only: %i[index]
+  resources :maps, only: [:index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
