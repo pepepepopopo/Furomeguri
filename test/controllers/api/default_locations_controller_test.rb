31375da241fd10ad45bfa6cb602161b4bb473b9b
rq-pepepepopopo-at-321899771753 <timeflies325@gmail.com>
2025-06-01 17:41:32 +0900

    fix:Linterror

--- test/controllers/api/default_locations_controller_test.rb
+++ test/controllers/api/default_locations_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class Api::DefaultLocationsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get '/api/default_locations'
+    get "/api/default_locations"
     assert_response :success
   end
 end
