d74be92f4c62435bd7b5d23308ed8ffcde8b07d5
rq-pepepepopopo-at-321899771753 <timeflies325@gmail.com>
2025-06-03 00:07:04 +0900

    fix:db_email_error

--- db/schema.rb
+++ db/schema.rb
@@ -53,11 +53,11 @@ ActiveRecord::Schema[7.2].define(version: 2025_06_01_021253) do
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "email"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
