2c898c2ad3fff9f7cf152a354fdaa7578a2fd801
rq-pepepepopopo-at-321899771753 <timeflies325@gmail.com>
2025-06-19 17:01:59 +0900

    test_controller未作成のためskip処理追記

--- test/controllers/itinerary_blocks_controller_test.rb
+++ test/controllers/itinerary_blocks_controller_test.rb
@@ -1,12 +1,14 @@
 require "test_helper"
 
 class ItineraryBlocksControllerTest < ActionDispatch::IntegrationTest
-  test "should get create", skip: "test未書き換え" do
+  test "should get create" do
+    skip "test未書き換え"
     get itinerary_itinerary_blocks_url(@itinerary)
     assert_response :success
   end
 
-  test "should get destroy", skip: "test未書き換え" do
+  test "should get destroy" do
+    skip "test未書き換え"
     get itinerary_itinerary_block_url(@itinerary, @block)
     assert_response :success
   end
