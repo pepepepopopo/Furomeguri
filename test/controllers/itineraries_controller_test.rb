dc489fb017e8b8f0e45da3f7f99d7d06453b0eaa
rq-pepepepopopo-at-321899771753 <timeflies325@gmail.com>
2025-06-19 16:36:38 +0900

    add:test_skip

--- test/controllers/itineraries_controller_test.rb
+++ test/controllers/itineraries_controller_test.rb
@@ -2,32 +2,38 @@ require "test_helper"
 
 class ItinerariesControllerTest < ActionDispatch::IntegrationTest
 
-  test "should get index", skip:"test未書き換え" do
+  test "should get index" do
+    skip "test未書き換え"
     get itineraries_url
     assert_response :success
   end
 
-  test "should get new", skip:"test未書き換え" do
+  test "should get new" do
+    skip "test未書き換え"
     get new_itinerary_url
     assert_response :success
   end
 
-  test "should get create", skip:"test未書き換え" do
+  test "should get create" do
+    skip "test未書き換え"
     post new_itinerary_url
     assert_response :success
   end
 
-  test "should get edit", skip:"test未書き換え" do
+  test "should get edit" do
+    skip "test未書き換え"
     get edit_itinerary_url(@itinerary)
     assert_response :success
   end
 
-  test "should get update", skip:"test未書き換え" do
+  test "should get update" do
+    skip "test未書き換え"
     patch itinerary_url(@itinerary)
     assert_response :success
   end
 
-  test "should get show", skip:"test未書き換え" do
+  test "should get show" do
+    skip "test未書き換え"
     get itinerary_url(@itinerary)
     assert_response :success
   end
