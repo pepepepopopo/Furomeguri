c383904ffb87f82f16e9c4cd5ac2d7896e5c9525
rq-pepepepopopo-at-321899771753 <timeflies325@gmail.com>
2025-06-22 18:20:36 +0900

    itineraryのtitleを入力必須とした

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_06_16_124853) do
+ActiveRecord::Schema[7.2].define(version: 2025_06_22_091350) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,7 +24,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_06_16_124853) do
 
   create_table "itineraries", force: :cascade do |t|
     t.bigint "user_id"
-    t.text "title"
+    t.text "title", default: "タイトル未定", null: false
     t.text "subtitle"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
