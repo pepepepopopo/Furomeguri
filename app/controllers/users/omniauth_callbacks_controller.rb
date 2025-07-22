043347fe5406d509d2c4a0f9f1a43b188ab30341
rq-pepepepopopo-at-321899771753 <timeflies325@gmail.com>
2025-07-22 19:38:39 +0900

    コンフリクトを解消

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -12,7 +12,10 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       sign_in_and_redirect @user, event: :authentication
       set_flash_message(:notice, :success, kind: provider.to_s.capitalize) if is_navigational_format?
     else
+<<<<<<< Updated upstream
       flash[:alert] = @user.errors.full_messages.to_sentence if @user.errors.any?
+=======
+>>>>>>> Stashed changes
       session["devise.#{provider}_data"] = request.env['omniauth.auth'].except(:extra)
       redirect_to new_user_registration_url
     end
@@ -21,4 +24,8 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def failure
     redirect_to root_path
   end
+<<<<<<< Updated upstream
 end
+=======
+end
+>>>>>>> Stashed changes
