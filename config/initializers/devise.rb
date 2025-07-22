ff4ba5ebe7fc5085092b4760e5afe08300bda418
rq-pepepepopopo-at-321899771753 <timeflies325@gmail.com>
2025-07-22 19:43:45 +0900

    fix:linterror

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -305,7 +305,7 @@ Devise.setup do |config|
   config.responder.error_status = :unprocessable_entity
   config.responder.redirect_status = :see_other
 
-  config.omniauth :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
+  config.omniauth :google_oauth2, `ENV.fetch('GOOGLE_CLIENT_SECRET')`, `ENV.fetch('GOOGLE_CLIENT_ID')`
 
   # ==> Configuration for :registerable
 
