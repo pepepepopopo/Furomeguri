eb23a699945e7ff53bd505f266cd5d3ecbff00f3
rq-pepepepopopo-at-321899771753 <timeflies325@gmail.com>
2025-07-26 13:22:35 +0900

    rubocop error fix

--- spec/factories/itineraries.rb
+++
@@ -1,5 +0,0 @@
-FactoryBot.define do
-  factory :itinerary do
-    
-  end
-end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,20 +2,18 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe "バリデーションチェック"
-    it "すべてのバリデーションが機能しているか" do
-      user = build(:user)
-      expect(user).to be_valid
-      expect(user.errors).to be_empty
-    end
-
-    it "emailがない場合にinvalidになる" do
-      user_without_email = build(:user, email:"")
-      expect(user_without_email).to be_invalid
-    end
-
-    it "emailに被りがある場合invalidになる" do
-      user = create(:user)
-      user_with_another_email = build(:user, email: user.email)
-      expect(user_with_another_email).to be_invalid
-    end
+  it "すべてのバリデーションが機能しているか" do
+    user = build(:user)
+    expect(user).to be_valid
+    expect(user.errors).to be_empty
+  end
+  it "emailがない場合にinvalidになる" do
+    user_without_email = build(:user, email: "")
+    expect(user_without_email).to be_invalid
+  end
+  it "emailに被りがある場合invalidになる" do
+    user = create(:user)
+    user_with_another_email = build(:user, email: user.email)
+    expect(user_with_another_email).to be_invalid
+  end
 end
