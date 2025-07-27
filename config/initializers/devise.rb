8276f48cc65ae343ccfb2b217dce7a816eec2d2b
rq-pepepepopopo-at-321899771753 <timeflies325@gmail.com>
2025-07-27 16:08:11 +0900

    CSFRエラー質問フォーム用リクエスト

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -313,7 +313,4 @@ Devise.setup do |config|
   # config.sign_in_after_change_password = true
 end
 
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV('GOOGLE_CLIENT_ID'), ENV('GOOGLE_CLIENT_SECRET')
-end
 OmniAuth.config.allowed_request_methods = %i[get]
