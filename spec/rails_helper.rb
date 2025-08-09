386dcc57cc636252efe4a0fc8cfdfccfb76b22c0
rq-pepepepopopo-at-321899771753 <timeflies325@gmail.com>
2025-08-10 00:26:08 +0900

    CI時にchromeをheadlessで起動

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -26,11 +26,18 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
 
   config.before(:each, type: :system) do
-    driven_by :remote_chrome
-    Capybara.server_host = IPSocket.getaddress(Socket.gethostname)
-    Capybara.server_port = 4444
-    Capybara.app_host = "http://#{Capybara.server_host}:#{Capybara.server_port}"
-    Capybara.ignore_hidden_elements = false
+    if ENV['CI']
+      driven_by :selenium_chrome_headless
+      Capybara.server_host = '127.0.0.1'
+      Capybara.server_port = nil
+      Capybara.app_host     = nil
+    else
+      driven_by :remote_chrome
+      Capybara.server_host = IPSocket.getaddress(Socket.gethostname)
+      Capybara.server_port = 4444
+      Capybara.app_host = "http://#{Capybara.server_host}:#{Capybara.server_port}"
+      Capybara.ignore_hidden_elements = false
+    end
   end
 
   config.include Devise::Test::IntegrationHelpers, type: :request
