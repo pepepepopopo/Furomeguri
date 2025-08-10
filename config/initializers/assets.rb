498f7071bd010580e0f317a6c2c3e46bcc1fcd53
rq-pepepepopopo-at-321899771753 <timeflies325@gmail.com>
2025-08-10 12:48:18 +0900

    npmからyarnにCIを変更

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,8 +1,6 @@
-
 Rails.application.config.assets.version = "1.0"
 
 Rails.application.config.assets.precompile += %w[application.css]
 
 # config/initializers/assets.rb
 Rails.application.config.assets.paths << Rails.root.join("app/assets/builds")
-
