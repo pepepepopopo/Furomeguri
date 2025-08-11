c059265c296cfcef4c296d9118cb8c384231cfbd
rq-pepepepopopo-at-321899771753 <timeflies325@gmail.com>
2025-08-12 08:42:18 +0900

    google認証による新規登録、ログインを実装(test環境のみ)

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,11 +16,10 @@ class User < ApplicationRecord
 
   def self.from_omniauth(auth)
     find_or_create_by(provider: auth.provider, uid: auth.uid) do |user|
-        user.email = auth.info.email
-        user.password = Devise.friendly_token[0, 20]
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
     end
   end
-
 
   def self.create_unique_string
     SecureRandom.uuid
