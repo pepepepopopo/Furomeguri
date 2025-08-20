d6a2d87da3fdc047a3f6358b1244724c428f8f51
rq-pepepepopopo-at-321899771753 <timeflies325@gmail.com>
2025-08-20 23:09:19 +0900

    tailwindcssv3 -> v4,add daisyUI

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,25 +1,18 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+// const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   content: [
-    './public/*.html',
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+    '../public/*.html',
+    '../app/helpers/**/*.rb',
+    '../app/javascript/**/*.js',
+    '../app/views/**/*.{erb,haml,html,slim}'
   ],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+//         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
-      colors: {
-        'green-light' : 'A3C9A8'
-      }
     },
   },
-  plugins: [
-    // require('@tailwindcss/forms'),
-    // require('@tailwindcss/typography'),
-    // require('@tailwindcss/container-queries'),
-  ]
+  plugins: []
 }
